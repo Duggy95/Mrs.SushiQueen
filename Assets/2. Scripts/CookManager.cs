c1e14ae62f0dda4e0131dbbb7e41f25b14786727
icis6791 <icis6791@naver.com>
2023-07-07 12:45:28 +0900

    요리전까지

---
+++ Assets/2. Scripts/CookManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CookManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GoEndScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
--- Assets/2. Scripts/FishingManager.cs
+++ Assets/2. Scripts/FishingManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class FishingManager : MonoBehaviour, IPointerClickHandler
 {
@@ -12,5 +13,10 @@ public class FishingManager : MonoBehaviour, IPointerClickHandler
     {
         Vector2 clickPos = eventData.position;
         var fishHpBar = Instantiate(hpBarPrefab, clickPos, new Quaternion(0, 0, 0, 0));
+    }
+
+    public void GoCookScene()
+    {
+        SceneManager.LoadScene(2);
     }
 }
