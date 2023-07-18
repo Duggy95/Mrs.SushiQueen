9883ecae71eb2374b80c91931d85931bc88da13d
Duggy95 <124519449+Duggy95@users.noreply.github.com>
2023-07-18 19:11:53 +0900

    GPGS

--- Assets/2. Scripts/Cook/DragSushi.cs
+++ Assets/2. Scripts/Cook/DragSushi.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static UnityEditor.Progress;
 
 public class DragSushi : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
